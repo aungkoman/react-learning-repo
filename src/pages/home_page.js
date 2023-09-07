ac511d58c7fc41db8760e70fd5ccbb7568b01624
Aung Ko Man <aungkoman@gmail.com>
2023-09-07 19:07:14 +0630

    bug list added

--- src/pages/home_page.js
+++ src/pages/home_page.js
@@ -7,6 +7,8 @@ const HomePage = () => {
     const [taskList, setTaskList] = useState([]);
 
     // for initial call
+    // BUG: အခုထိတော့ (၂) ကော ခေါ်နေတုန်း။
+    
     useEffect(() => {
         console.log("home_page->useEffect");
         axios.get(`https://60fbca4591156a0017b4c8a7.mockapi.io/fakeData`)
