f8d3dbf5ded91a31d748346468bb85f43718e22e
Aung Ko Man <aungkoman@gmail.com>
2023-09-07 19:55:50 +0630

    login function added

--- src/pages/auth/login_page.js
+++ src/pages/auth/login_page.js
@@ -1,5 +1,49 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+
 const LoginPage = () => {
-    return <h1>Login Page</h1>;
+    // State
+    // let history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // core
+    const navigate = useNavigate();
+
+    // State Managers
+    const login = (event) => {
+        event. preventDefault();
+        let body  = {
+            email : email,
+            password : password
+        };
+        axios.post(`http://localhost/qrmenu/public/api/v1/login`, body )
+        .then((response) => {
+            console.log("login->response");
+            console.log(response);
+            console.log(response.data);
+            // navigate('/home');
+            // history.push('/home')
+            // how to set user info to state management
+        })
+        .catch((err) => {
+            console.log("login->err");
+            console.log(err);
+            console.log(err.response.data);
+        });
+    }
+
+    // UI
+    return <>
+        <h2>Login Page</h2>
+        <form>
+            <input type="email" placeholder="enter email" onChange={(e) => setEmail(e.target.value)} required />
+            <input type="password" placeholder="enter password" onChange={(e) => setPassword(e.target.value)}  required />
+            <input type="submit" onClick={login} value="Login" />
+        </form>
+    </>
   };
   
 export default LoginPage;
