dac18079b6688eda93d4f4a9babfd073d8a5dbc9
Aung Ko Man <aungkoman@gmail.com>
2023-09-07 14:31:05 +0630

    React Routing added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ import ToDoListSection from './home-page/to-do-list-section';
 import ReactIntroSection from './home-page/react-intro-section';
 import ConversationListSection from './chat/conversation-list-section';
 
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/layout";
+import HomePage from "./pages/home_page";
+import ProfilePage from "./pages/profile_page";
+import NoPage from "./pages/no_page";
+
 function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="blogs" element={<ProfilePage />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+  /*
   return (
     <>
       <div className="MainContainer">
@@ -14,6 +33,7 @@ function App() {
       </div>
     </>
   );
+  */
 }
 
 export default App;
