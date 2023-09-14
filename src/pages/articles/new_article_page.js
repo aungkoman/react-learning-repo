dd692dbb861f4d303b524cc1a9ea6ee406229edc
Aung Ko Man <aungkoman@gmail.com>
2023-09-14 17:09:57 +0630

    New Article Form အတွက် UI နဲ့ API ရပြီ

---
+++ src/pages/articles/new_article_page.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import {login} from "../../providers/actions/UserAction";
+
+import { connect } from 'react-redux';
+
+
+/* ဒီမှာ ဘာတွေ လိုမလဲ? */
+/*
+    access token , i means user info တစ်ခုပဲ​လိုမယ်။
+    // listen လုပ်တာလဲ ဖြစ်ကောင်းဖြစ်မယ်
+    API ခေါ်တဲ့အခါမှ သုံးတာလဲ​ဖြစ်မယ်။
+*/
+
+/*
+    @required state / reducer
+    - user
+
+    @required action
+    - addNewArticle
+*/
+const NewArticlePage = ({user, addNewArticle}) => {
+    // State
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    // core
+    const navigate = useNavigate();
+
+    // State Managers
+    const submitArticle = (event) => {
+        console.log("NewArticlePage->submitArticle");
+        event.preventDefault();
+
+        let body  = {
+            title : title,
+            content : content
+        };
+        // TODO: ဒီကောင်ကလည်း Reducer တစ်ခုခုမှာ သတ်မှတ်ထားတာတောင် ပိုကောင်းမယ်။
+        // သုံးမယ့် Endpoint တွေ အကုန်လုံး။
+        // add auth headers
+        const token =  user.access_token;
+        console.log("token is ", token);
+        const axiosInstance = axios.create({
+            baseURL: "http://localhost/pandora/public/api/v1",
+            headers: {
+              'Authorization': `Bearer ${token}`,
+              'Content-Type': 'application/json', // You can set other headers as needed
+            },
+        });
+        axiosInstance.post(`/articles`, body )
+        .then((response) => {
+            console.log("NewArticlePage->submitArticle->response");
+            console.log(response);
+            console.log(response.data);
+            // navigate('/home');
+            // history.push('/home')
+            // how to set user info to state management
+            let response_data = response.data;
+            if(response_data.status == true){
+                let new_article = response_data.data;
+                console.log("new_article is ");
+                console.log(new_article);
+                // new article တစ်ခု article list မှာ ထည့်။
+                // ဆိုတော့ dispatch လုပ်ပေ့ါ။
+                // မဟုတ်လည်း Action Function တစ်ခု ခေါ်လည်း ရတယ်။
+                addNewArticle(user);
+                // go to article list
+                navigate('/articles');
+            }
+        })
+        .catch((err) => {
+            console.log("NewArticlePage->submitArticle->err");
+            console.log(err);
+            console.log(err.response.data);
+        });
+    }
+
+    // UI
+    return <>
+        <h2>Create New Article Page</h2>
+        <form>
+            <input type="text" placeholder="enter email" onChange={(e) => setEmail(e.target.value)} required />
+            {/* text area */}
+            <input type="submit" onClick={submitArticle} value="Submit" />
+        </form>
+    </>
+};
+  
+// export default LoginPage;
+export default connect()(NewArticlePage);
