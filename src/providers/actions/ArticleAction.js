0030b4fe19e62fc27b52e910344fc83277c57291
Aung Ko Man <aungkoman@gmail.com>
2023-09-14 17:11:50 +0630

    add new article action added

--- src/providers/actions/ArticleAction.js
+++ src/providers/actions/ArticleAction.js
@@ -2,6 +2,9 @@ export const addAll = articles => ({
     type: 'ADD',
     articles: articles
 });
+export const addNewArticle = () =>({
+    type: 'ADD_NEW'
+});
 export const clearAll = () =>({
     type: 'CLEAR'
 });
