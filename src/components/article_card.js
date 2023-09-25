da71de74f90b6e2dbe54edcdd8bda1dd038cfdeb
Aung Ko Man <aungkoman@gmail.com>
2023-09-25 10:09:39 +0630

    up vote button added

--- src/components/article_card.js
+++ src/components/article_card.js
@@ -17,31 +17,35 @@ const ArticleCard = ({article, upVote, unVote, downVote}) => {
             <p> { article.content } </p>
         </div>
         <div className="article-card-footer">
-            this is footer
+            <button>Up</button>
+            <span>45</span>
+            <button>Down</button>
+            {/* need to go to detail page */}
+            <Link to={ "/comments?article_id="+ article.id } >( {article.comment_count} ) Comments</Link>
         </div>
-
-        <hr></hr>
-        <h2 >{ article.title }</h2>
-        <p> { article.content } </p>
-        <span>{article.user_vote}</span>
-        {/* 
-            ဒီ Function ကို onClick မှ ခေါ်ပါဆိုတာကို ဘယ်လိုလုပ်ကြမလဲ?
-            Up Vote ခလုပ်က အခြေအနေ နှစ်ခု မှာ ရှိနိုင်တယ်။
-            user_vote က 1 ဒါမှမဟုတ် အခြား တစ်ခုခု
-        */}
-        { article.user_vote == 1 
-            ? <button onClick={ () => unVote(article.id) } > Un Vote </button>
-            : <button onClick={ () => upVote(article.id) } > Up Vote </button>
-        }
-        <span>({ article.up_vote}) Up Vote(s)</span>
-        
-        <Link to={ "/comments?article_id="+ article.id } >( {article.comment_count} ) Comments</Link>
-        { article.user_vote == 0 
-            ? <button onClick={ () => unVote(article.id) } > Un Vote </button>
-            : <button onClick={ () => downVote(article.id) } > Down Vote </button>
-        }
-        <span>({ article.down_vote}) Down Vote(s)</span>
         
     </div>
 };
 export default ArticleCard;
+
+// <hr></hr>
+//         <h2 >{ article.title }</h2>
+//         <p> { article.content } </p>
+//         <span>{article.user_vote}</span>
+//         {/* 
+//             ဒီ Function ကို onClick မှ ခေါ်ပါဆိုတာကို ဘယ်လိုလုပ်ကြမလဲ?
+//             Up Vote ခလုပ်က အခြေအနေ နှစ်ခု မှာ ရှိနိုင်တယ်။
+//             user_vote က 1 ဒါမှမဟုတ် အခြား တစ်ခုခု
+//         */}
+//         { article.user_vote == 1 
+//             ? <button onClick={ () => unVote(article.id) } > Un Vote </button>
+//             : <button onClick={ () => upVote(article.id) } > Up Vote </button>
+//         }
+//         <span>({ article.up_vote}) Up Vote(s)</span>
+        
+//         <Link to={ "/comments?article_id="+ article.id } >( {article.comment_count} ) Comments</Link>
+//         { article.user_vote == 0 
+//             ? <button onClick={ () => unVote(article.id) } > Un Vote </button>
+//             : <button onClick={ () => downVote(article.id) } > Down Vote </button>
+//         }
+//         <span>({ article.down_vote}) Down Vote(s)</span>
